4841cd35563eedc54e10ff6a0dcf2122a861538d
brendon cecilio <brendon.cecilio@gmail.com>
2019-10-29 11:12:27 -0400

    Initial Commit

---
+++ FLashlight3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLashlight3
+//
+//  Created by Brendon Cecilio on 10/29/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
